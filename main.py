fee9f714c0ef1cd30473af7900dae3de75e3483f
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2021-10-06 11:12:16 +1100

    UPDATE main.py - implement working base for getting files

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+from urllib.request import Request, urlopen
+from urllib.error import URLError
+from pprint import pprint
 
+URL_BASE = 'https://raw.githubusercontent.com/github/gitignore/master/'
+GITIGNORE_FILE = '.gitignore'
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_remote_gitignore(language):
+    url = f'{URL_BASE}{language}{GITIGNORE_FILE}'
+    req = Request(url)
+
+    try:
+        response = urlopen(req)
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            print('We failed to reach a server.')
+            print('Reason: ', e.reason)
+        elif hasattr(e, 'code'):
+            print('The server couldn\'t fulfill the request.')
+            print('Error code: ', e.code)
+    else:
+        pprint(response.read())
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    get_remote_gitignore("python")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
